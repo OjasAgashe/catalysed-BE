e64f5001aa728034671b43df2a80b004fc8c63a7
Ojas Agashe <ojas.v.agashe@gmail.com>
2021-01-14 19:27:37 +0530

    Adding Email support

---
+++ src/main/java/com/ojas/gcp/firstappenginetryout/config/MailTemplateConfig.java
@@ -0,0 +1,46 @@
+package com.ojas.gcp.firstappenginetryout.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.Collections;
+
+@Configuration
+public class MailTemplateConfig {
+    public static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
+
+//    @Bean
+    public ResourceBundleMessageSource emailMessageSource() {
+        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("MailMessages");
+        return messageSource;
+    }
+
+    @Bean("htmlTemplateResolver")
+    public TemplateEngine emailTemplateEngine() {
+        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        // Resolver for HTML emails (except the editable one)
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        // Message source, internationalization specific to emails
+        templateEngine.setTemplateEngineMessageSource(this.emailMessageSource());
+        return templateEngine;
+    }
+
+    private ITemplateResolver htmlTemplateResolver() {
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setOrder(Integer.valueOf(1));
+        templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
+        templateResolver.setPrefix("/templates/mail/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(EMAIL_TEMPLATE_ENCODING);
+        templateResolver.setCacheable(false);
+        return templateResolver;
+    }
+}
