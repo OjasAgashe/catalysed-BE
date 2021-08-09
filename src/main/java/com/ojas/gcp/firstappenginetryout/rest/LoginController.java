38a28b836728a462701d62989cdff46848f39fa0
Ojas Agashe <ojas.v.agashe@gmail.com>
2021-08-10 02:59:39 +0530

    Adding User name and Orgname in authenticate request response

--- src/main/java/com/ojas/gcp/firstappenginetryout/rest/LoginController.java
+++ src/main/java/com/ojas/gcp/firstappenginetryout/rest/LoginController.java
@@ -4,16 +4,22 @@ import com.ojas.gcp.firstappenginetryout.auth.AppUserDetailsService;
 import com.ojas.gcp.firstappenginetryout.auth.AuthenticationRequest;
 import com.ojas.gcp.firstappenginetryout.auth.AuthenticationResponse;
 import com.ojas.gcp.firstappenginetryout.auth.SessionUser;
+import com.ojas.gcp.firstappenginetryout.entity.enums.UserType;
 import com.ojas.gcp.firstappenginetryout.rest.dto.AuthenticationUserDTO;
+import com.ojas.gcp.firstappenginetryout.rest.dto.OrgMetaDTO;
 import com.ojas.gcp.firstappenginetryout.security.JwtUtils;
+import com.ojas.gcp.firstappenginetryout.service.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.xml.bind.ValidationException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/authenticate")
@@ -23,6 +29,8 @@ public class LoginController {
     private AuthenticationManager authenticationManager;
     @Autowired
     private AppUserDetailsService userDetailsService;
+    @Autowired
+    private OrganizationService organizationService;
     @Autowired
     private JwtUtils jwtTokenUtils;
 
@@ -43,14 +51,29 @@ public class LoginController {
         return ResponseEntity.ok(new AuthenticationResponse(generateAuthUserDTO(userDetails), jwt));
     }
 
-    private AuthenticationUserDTO generateAuthUserDTO(SessionUser sessionUser) {
-        return new AuthenticationUserDTO(
+    private AuthenticationUserDTO generateAuthUserDTO(SessionUser sessionUser) throws ValidationException {
+        AuthenticationUserDTO authUser =  new AuthenticationUserDTO(
                 sessionUser.getId(),
                 sessionUser.getEmailId(),
                 sessionUser.getUsername(),
+                null,
                 sessionUser.isEnabled(),
                 sessionUser.getUserType(),
                 sessionUser.isAccountVerified(),
-                sessionUser.isProfileCreated());
+                sessionUser.isProfileCreated()
+        );
+        if (!authUser.isProfileCreated()) {
+            if (authUser.getUserType() == UserType.ORGANIZATION_USER) {
+                OrgMetaDTO org = organizationService.getOrgMetaForOrgUser(sessionUser);
+                authUser.setOrgName(org.getName());
+            } else {
+                List<OrgMetaDTO> orgs = organizationService.getConnectedOrgs(sessionUser.getId());
+                if (CollectionUtils.isEmpty(orgs)) {
+                    throw new ValidationException("User not connected to any organization");
+                }
+                authUser.setOrgName(orgs.get(0).getName());
+            }
+        }
+        return authUser;
     }
 }
